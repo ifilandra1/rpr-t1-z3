c3a8d74d4e095575664be72e6304a7b9e92a9228
ifilandra1 <ifilandra1@etf.unsa.ba>
2018-10-16 18:33:17 +0200

    Treci zadatak

---
+++ rpr/etf/unsa/ba/Main.java
@@ -0,0 +1,22 @@
+package rpr.etf.unsa.ba;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Sat s=new Sat(15,30,45);
+        s.Ispisi();
+        s.Sljedeci();
+        s.Ispisi();
+        s.PomjeriZa(-48);
+        s.Ispisi();
+        s.Postavi(0,0,0);
+        s.Ispisi();
+
+
+    }
+}
+
+
+
